96026536662b81250716a87dd47081d0ffa0d7e3
Jacob Rosenthal <jakerosenthal@gmail.com>
2017-11-26 22:46:23 -0700

    carthage framework scheme

---
+++ AVCapture/AVCapture/AVCapture.h
@@ -0,0 +1,21 @@
+//
+//  AVCapture.h
+//  AVCapture
+//
+//  Created by Jacob Rosenthal on 11/26/17.
+//  Copyright © 2017 augmentous. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for AVCapture.
+FOUNDATION_EXPORT double AVCaptureVersionNumber;
+
+//! Project version string for AVCapture.
+FOUNDATION_EXPORT const unsigned char AVCaptureVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AVCapture/PublicHeader.h>
+
+#import <AVCapture/AVRecorderDocument.h>
+#import <AVCapture/AVFrameRateRange_AVRecorderAdditions.h>
+#import <AVCaptureDeviceFormat_AVRecorderAdditions.h>
